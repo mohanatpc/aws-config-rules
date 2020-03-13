82a1bbaa306d579656a66d21db64187b026b1184
annamm <annamm@amazon.com>
2020-03-13 14:18:32 +0530

    code review comments implemented

--- python/RDS_SNAPSHOT_ENCRYPTED/RDS_SNAPSHOT_ENCRYPTED.py
+++ python/RDS_SNAPSHOT_ENCRYPTED/RDS_SNAPSHOT_ENCRYPTED.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may
 # not use this file except in compliance with the License. A copy of the License is located at
@@ -12,52 +12,72 @@
 #####################################
 ##           Gherkin               ##
 #####################################
+
 Rule Name:
   RDS_SNAPSHOT_ENCRYPTED
 
 Description:
-  Check whether AWS RDS Snapshot is encrypted.
+  Check whether Amazon RDS DB Snapshots for the Amazon RDS DB Instances are encrypted. The rule is NON_COMPLIANT, if the DB Snapshot is not encrypted.
 
 Rationale:
-  Encrypting a  AWS RDS Snapshot with AWS Key Management Service(KMS) keys ensures protection.
+  Encrypting a  Amazon RDS DB Snapshot with AWS Key Management Service(KMS) keys ensures protection.
 
 Indicative Severity:
   Medium
 
 Trigger:
-  Configuration Change on AWS::RDS::DBSnapshot and AWS::RDS::DBClusterSnapshot
+  Configuration Change on AWS::RDS::DBSnapshot or AWS::RDS::DBClusterSnapshot
 
 Reports on:
-  AWS::RDS::DBSnapshot and AWS::RDS::DBClusterSnapshot
+  AWS::RDS::DBSnapshot or AWS::RDS::DBClusterSnapshot
 
 Rule Parameters:
-  RDSSnapshotIdentifier
-   (Optional) Enter the RDS Snapshot InstanceIdentifier to
-   display snapshots are COMPLIANT  (or list of ID separated by ",")
+  kmsKeyId(Optional) : AWS KMS key id used for encryption
+
 
 Scenarios:
-  Scenario: 2
-    Given: AWS RDS Snapshot available and is encrypted with AWS KMS
+  Scenario: 1
+    Given: Amazon RDS DB Snapshot available and is encrypted with AWS KMS
+    And: parameter 'kmsKeyId' is not provided
     Then: Return COMPLIANT
-  Scenario: 3
-    Given:  AWS RDS Snapshot available and is not encrypted  with AWS KMS
+  Scenario: 2
+    Given: Amazon RDS DB Snapshot available and is not encrypted with AWS KMS
     Then: Return NON_COMPLIANT
+  Scenario: 3
+    Given: Amazon RDS DB Snapshot available and is encrypted with AWS KMS
+    And: parameter 'kmsKeyId' is provided
+    And: parameter 'kmsKeyId' matched with 'kmsKeyId' in configuration item
+    Then: Return COMPLIANT
+  Scenario: 4
+    Given: Amazon RDS DB Snapshot available and is encrypted with AWS KMS
+    And: parameter 'kmsKeyId' is provided
+    And: parameter 'kmsKeyId' not matched with 'kmsKeyId' in configuration item
+    Then: Return NON_COMPLIANT
+
 same scenarios for DB clusters
 """""
 
 from rdklib import Evaluator, Evaluation, ConfigRule, ComplianceType
 
 
-class RdsSnapshotEncrypted(ConfigRule):
+class RDS_SNAPSHOT_ENCRYPTED(ConfigRule):
 
     def evaluate_change(self, event, client_factory, configuration_item, valid_rule_parameters):
-        response = configuration_item['configuration'].get('encrypted')
-        if response is not None and response:
+        encryption_enabled = configuration_item['configuration'].get('encrypted')
+        # print("Rule Parameters : "+str(valid_rule_parameters))
+        if encryption_enabled:
+            if valid_rule_parameters and valid_rule_parameters.get("kmsKeyId"):
+                rds_kms_keyid = configuration_item['configuration'].get('kmsKeyId')
+                if rds_kms_keyid and rds_kms_keyid == valid_rule_parameters.get("kmsKeyId"):
+                    return [Evaluation(ComplianceType.COMPLIANT)]
+                return [Evaluation(ComplianceType.NON_COMPLIANT,
+                                   annotation="RDS snapshot encrypted KMS key id"
+                                              " does not match with configured KMS key id")]
             return [Evaluation(ComplianceType.COMPLIANT)]
-        return [Evaluation(ComplianceType.NON_COMPLIANT, annotation="Encryption is not enabled for snapshot")]
+        return [Evaluation(ComplianceType.NON_COMPLIANT, annotation="Encryption is not enabled for this DB snapshot")]
 
 
 def lambda_handler(event, context):
-    my_rule = RdsSnapshotEncrypted()
+    my_rule = RDS_SNAPSHOT_ENCRYPTED()
     evaluator = Evaluator(my_rule)
     return evaluator.handle(event, context)
--- python/RDS_SNAPSHOT_ENCRYPTED/RDS_SNAPSHOT_ENCRYPTED_test.py
+++ python/RDS_SNAPSHOT_ENCRYPTED/RDS_SNAPSHOT_ENCRYPTED_test.py
@@ -26,12 +26,14 @@ RESOURCE_TYPE = 'AWS::::Account'
 #############
 
 MODULE = __import__('RDS_SNAPSHOT_ENCRYPTED')
-RULE = MODULE.RdsSnapshotEncrypted()
+RULE = MODULE.RDS_SNAPSHOT_ENCRYPTED()
 
 CLIENT_FACTORY = MagicMock()
 
 # example for mocking S3 API calls
 RDS_CLIENT_MOCK = MagicMock()
+
+RULE_PARAMETERS = {'kmsKeyId': 'arn:aws:kms:ap-south-1:653566069331:key/2fae0d44-1951-421d-ba9d-2e2e759b577c'}
 
 
 def mock_get_client(client_name, *args, **kwargs):
@@ -39,21 +41,36 @@ def mock_get_client(client_name, *args, **kwargs):
         return RDS_CLIENT_MOCK
     raise Exception("Attempting to create an unknown client")
 
-
 @patch.object(CLIENT_FACTORY, 'build_client', MagicMock(side_effect=mock_get_client))
 class ComplianceTest(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_rds_snapshot_encrypted(self):
-        configuration = {"configuration":{"encrypted": True}}
+        configuration = {"configuration": {"encrypted": True}}
         response = RULE.evaluate_change({}, {}, configuration, {})
         resp_expected = [Evaluation(ComplianceType.COMPLIANT)]
         rdklibtest.assert_successful_evaluation(self, response, resp_expected)
 
     def test_rds_snapshot_not_encrypted(self):
-        configuration = {"configuration":{"encrypted": False}}
+        configuration = {"configuration": {"encrypted": False}}
         response = RULE.evaluate_change({}, {}, configuration, {})
-        resp_expected = [Evaluation(ComplianceType.NON_COMPLIANT, annotation="Encryption is not enabled for snapshot")]
+        resp_expected = [Evaluation(ComplianceType.NON_COMPLIANT,
+                                    annotation="Encryption is not enabled for this DB snapshot")]
+        rdklibtest.assert_successful_evaluation(self, response, resp_expected)
+
+    def test_rds_snapshot_encrypted_kms_key_id_matched(self):
+        configuration = {"configuration": {"encrypted": True,
+                                           "kmsKeyId": "arn:aws:kms:ap-south-1:653566069331:key/"
+                                                       "2fae0d44-1951-421d-ba9d-2e2e759b577c"}}
+        response = RULE.evaluate_change({}, {}, configuration, RULE_PARAMETERS)
+        resp_expected = [Evaluation(ComplianceType.COMPLIANT)]
+        rdklibtest.assert_successful_evaluation(self, response, resp_expected)
+
+    def test_rds_snapshot_encrypted_kms_key_id_not_matched(self):
+        configuration = {"configuration": {"encrypted": True,
+                                           "kmsKeyId": "arn:aws:kms:ap-south-1:653566069331:key/"
+                                                       "2fae0d44-1951-421d-ba9d-2e2e759b588c"}}
+        response = RULE.evaluate_change({}, {}, configuration, RULE_PARAMETERS)
+        resp_expected = [Evaluation(ComplianceType.NON_COMPLIANT,
+                                    annotation="RDS snapshot encrypted KMS key id "
+                                               "does not match with configured KMS key id")]
         rdklibtest.assert_successful_evaluation(self, response, resp_expected)
